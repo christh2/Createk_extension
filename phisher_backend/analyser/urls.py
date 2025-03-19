8903e24135d5915dcffb293d18eb8cc30e1770d1
JosiasDH <hanniel.degbelo@epitech.eu>
2025-03-19 13:33:46 +0100

    remove unused analyser app and related files

--- phisher_backend/analyser/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from analyser import views
-
-urlpatterns = [
-    path('phisher/', views.PhisherView.as_view(), name='phisher_view'),
-    path('phisher/analyze-url/', views.analyze_url, name='analyze_url')
-]
